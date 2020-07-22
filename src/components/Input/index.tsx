0a654a00940048dcab2043d4f890de81c4ce3266
asthemoon0 <47937104+asthe0@users.noreply.github.com>
2020-07-22 12:27:36 +0900

    initial commit

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from 'react';
+import { TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
+
+import { Container, TextInput, Icon } from './styles';
+
+interface InputProps extends TextInputProps {
+  name: string;
+  icon: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
+  const inputValueRef = useRef({ value: '' });
+  const { registerField, defaultValue, fieldName, error } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputValueRef.current,
+      path: 'value',
+    });
+  }, []);
+  return (
+    <Container>
+      <Icon name={icon} size={20} color="#666360" />
+      <TextInput
+        keyboardAppearance="dark"
+        placeholderTextColor="#666360"
+        defaultValue={defaultValue}
+        onChangeText={value => {
+          inputValueRef.current.value = value;
+        }}
+        {...rest}
+      />
+    </Container>
+  );
+};
+
+export default Input;
